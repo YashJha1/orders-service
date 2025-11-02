a90ffb833851e35dc4092ed1172de89579c16283
yashjha1 <rajsmarty899@gmail.com>
2025-11-02 14:41:38 +0000

    Updated CatalogClient.java

--- src/main/java/com/eci/orderservice/client/CatalogClient.java
+++ src/main/java/com/eci/orderservice/client/CatalogClient.java
@@ -21,6 +21,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "catalog-service", url = "http://localhost:8080/v1/products")
 public interface CatalogClient {
+    @GetMapping("/v1/products/{sku}")
+    ProductDto getProductBySku(@PathVariable("sku") String sku);
 
     @GetMapping("/products/{productId}")
     CatalogResponse getProductById(@PathVariable("productId") Long productId);
