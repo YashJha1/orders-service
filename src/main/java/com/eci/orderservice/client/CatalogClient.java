c5653d5b3339548f68fd53a3da6409612bf93996
yashjha1 <rajsmarty899@gmail.com>
2025-11-02 15:38:27 +0000

    Updated CatalogClient.java

--- src/main/java/com/eci/orderservice/client/CatalogClient.java
+++ src/main/java/com/eci/orderservice/client/CatalogClient.java
@@ -36,7 +36,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "catalog-service", url = "http://localhost:8080")
+@FeignClient(name = "catalog-service", url = "http://localhost:8080/v1/products")
 public interface CatalogClient {
 
     // Fetch product by SKU (used in orders)
